a801609e5335c828e7fd251c5cf8ef07b421d0a6
James <goldietg@hotmail.co.uk>
2023-06-24 21:07:48 +0100

    fixed bug in scrollbar when changing pages

--- TkUIController.py
+++ TkUIController.py
@@ -26,9 +26,6 @@ class TkApp(tk.Tk):
         self.scrollBar.pack(side=tk.RIGHT, fill=tk.Y)
 
         self.canvas.configure(yscrollcommand=self.scrollBar.set)
-        self.canvas.bind(
-            '<Configure>', lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all"))
-        )
 
         self.switchFrame(ImportPage)
 
@@ -37,6 +34,9 @@ class TkApp(tk.Tk):
         if self.frame is not None:
             self.frame.destroy()
         self.frame = newFrame
+        self.frame.bind(
+            '<Configure>', lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+        )
         self.canvas.create_window((0,0), window=self.frame, anchor="nw")
         #self.frame.grid(row=0, column=0, sticky="nsew")
 
