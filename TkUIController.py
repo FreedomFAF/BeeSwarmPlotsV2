d4beaeedbd4c20ab977666c025d2a75381782f56
James <goldietg@hotmail.co.uk>
2023-06-24 20:39:38 +0100

    add scroll bar

--- TkUIController.py
+++ TkUIController.py
@@ -16,6 +16,20 @@ class TkApp(tk.Tk):
         self.geometry("1200x1200")
         self.frame = None
 
+        self.mainFrame = tk.Frame(self)
+        self.mainFrame.pack(fill=tk.BOTH, expand=1)
+
+        self.canvas = tk.Canvas(self.mainFrame)
+        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+
+        self.scrollBar = tk.Scrollbar(self.mainFrame, orient=tk.VERTICAL, command=self.canvas.yview)
+        self.scrollBar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        self.canvas.configure(yscrollcommand=self.scrollBar.set)
+        self.canvas.bind(
+            '<Configure>', lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+        )
+
         self.switchFrame(ImportPage)
 
     def switchFrame(self, frameClass):
@@ -23,5 +37,6 @@ class TkApp(tk.Tk):
         if self.frame is not None:
             self.frame.destroy()
         self.frame = newFrame
-        self.frame.grid(row=0, column=0, sticky="nsew")
+        self.canvas.create_window((0,0), window=self.frame, anchor="nw")
+        #self.frame.grid(row=0, column=0, sticky="nsew")
 
